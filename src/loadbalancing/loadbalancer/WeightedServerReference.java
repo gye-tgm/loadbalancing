656e00896788b2bda5f86961ade513ceef8c5b2a
efrantar-tgm <efrantar@student.tgm.ac.at>
2014-12-12 13:05:35 +0100

    added initial structure of WRR

--- src/loadbalancing/loadbalancer/WeightedServerReference.java
+++ src/loadbalancing/loadbalancer/WeightedServerReference.java
@@ -4,9 +4,9 @@ package loadbalancing.loadbalancer;
  * Created by gary on 12/12/14.
  */
 public class WeightedServerReference extends ServerReference {
-    int weight;
+    private int weight;
 
-    public WeightedServerReference(int weight) {
-        this.weight = weight;
-    }
+    public WeightedServerReference(int weight) { this.weight = weight; }
+
+    public int getWeight() {return weight; }
 }
--- src/loadbalancing/loadbalancer/strategies/wrr/WRR.java
+++ src/loadbalancing/loadbalancer/strategies/wrr/WRR.java
@@ -1,7 +1,66 @@
 package loadbalancing.loadbalancer.strategies.wrr;
 
+import loadbalancing.Server;
+import loadbalancing.loadbalancer.ServerReference;
+import loadbalancing.loadbalancer.WeightedServerReference;
+import loadbalancing.loadbalancer.strategies.LoadBalancingStrategy;
+
+import java.util.ArrayList;
+
 /**
- *
+ * @author Elias Frantar
  */
-public class WRR {
+public class WRR implements LoadBalancingStrategy {
+
+    private class WRREntry {
+        Server server;
+        int weight;
+        int assigned;
+
+        public WRREntry(Server server, int weight) {
+            this.server = server;
+            this.weight = weight;
+            this.assigned = 0;
+        }
+    }
+
+    private ArrayList<WRREntry> list;
+    private int current;
+
+    public WRR() {
+        list = new ArrayList<>();
+        current = 0;
+    }
+
+
+    public void register(Server server, int weight) {
+        list.add(new WRREntry(server, weight));
+    }
+
+    @Override
+    public void increment(ServerReference server) {
+
+    }
+
+    @Override
+    public void decrement(ServerReference server) {
+
+    }
+
+    @Override
+    public ServerReference getNext() {
+        return null;
+    }
+
+    @Override
+    public void register(ServerReference server) {
+        WeightedServerReference wrrserver = (WeightedServerReference) server;
+        list.add(new WRREntry(wrrserver, wrrserver.getWeight()));
+    }
+
+    @Override
+    public void unregister(ServerReference server) {
+        list.remove(server);
+    }
+
 }
