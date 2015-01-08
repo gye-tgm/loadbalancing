19171abfc4bcbc7edba976c183760abd2771fe7a
Gary Ye <gye@student.tgm.ac.at>
2015-01-08 21:14:49 +0100

    Reading server references from XML config file.

--- src/loadbalancing/loadbalancer/LoadBalancer.java
+++ src/loadbalancing/loadbalancer/LoadBalancer.java
@@ -4,9 +4,19 @@ import loadbalancing.IServer;
 import loadbalancing.loadbalancer.strategies.LoadBalancingStrategy;
 import loadbalancing.loadbalancer.strategies.lcf.LCF;
 import loadbalancing.loadbalancer.strategies.lcf.LCFServerReference;
+import loadbalancing.loadbalancer.strategies.wrr.WRR;
+import loadbalancing.loadbalancer.strategies.wrr.WeightedServerReference;
 import org.apache.xmlrpc.WebServer;
-import org.apache.xmlrpc.XmlRpcException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -54,12 +64,75 @@ public class LoadBalancer extends Thread implements IServer {
         strategy.register(serverReference);
     }
 
+    public static void usage() {
+        System.out.println("usage: java loadbalancing.loadbalancer.LoadBalancer <config-file>");
+    }
+
     public static void main(String[] args){
-        LoadBalancer loadBalancer = new LoadBalancer(new LCF());
-
-        // TODO: get the slave servers from args[]
-        loadBalancer.register(new LCFServerReference("http://localhost:5050/xmlrpc"));
-
+        if (args.length != 1) {
+            usage();
+            return;
+        }
+        LoadBalancer loadBalancer = null;
+        try {
+            File file = new File(args[0]);
+            loadBalancer = readXML(file);
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            System.err.printf("Error occurred while reading the file: %s\n", args[0]);
+        } catch (StrategyNotFoundException e) {
+            System.err.println("Strategy not found! Choose from the following list: WRR, LCF");
+        }
         loadBalancer.start();
+        System.out.println("Load Balancer started...\n");
+    }
+
+    /**
+     * Reads the xml config file and returns the corresponding load balancer (with the registered server references)
+     *
+     * @param xmlFile the xml config file
+     * @return the loadbalancer
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws SAXException
+     * @throws StrategyNotFoundException
+     */
+    private static LoadBalancer readXML(File xmlFile) throws ParserConfigurationException, IOException, SAXException,
+            StrategyNotFoundException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(xmlFile);
+        doc.getDocumentElement().normalize();
+
+        LoadBalancer loadBalancer = null;
+        Element e = (Element) doc.getElementsByTagName("loadbalancing").item(0);
+        NodeList nodeList = doc.getElementsByTagName("slave");
+
+        String strategy = e.getElementsByTagName("strategyname").item(0).getTextContent();
+        if (strategy.equals("LCF")) {
+            loadBalancer = new LoadBalancer(new LCF());
+        } else if (strategy.equals("WRR")) {
+            loadBalancer = new LoadBalancer(new WRR());
+        } else {
+            throw new StrategyNotFoundException();
+        }
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                String url = element.getElementsByTagName("url").item(0).getTextContent();
+                if (strategy.equals("LCF")) {
+                    loadBalancer.register(new LCFServerReference(url));
+                } else if (strategy.equals("WRR")) {
+                    int weight = Integer.parseInt(element.getElementsByTagName("weight").item(0).getTextContent());
+                    loadBalancer.register(new WeightedServerReference(url, weight));
+                }
+            }
+        }
+
+        return loadBalancer;
+    }
+
+    static class StrategyNotFoundException extends Exception {
     }
 }
