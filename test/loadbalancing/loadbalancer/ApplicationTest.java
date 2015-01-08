6af1e7dd85abb7e129466a3fd25746472d95633f
efrantar-tgm <efrantar@student.tgm.ac.at>
2015-01-08 22:39:04 +0100

    fixed test-cases

--- test/loadbalancing/loadbalancer/ApplicationTest.java
+++ test/loadbalancing/loadbalancer/ApplicationTest.java
@@ -7,6 +7,7 @@ import loadbalancing.slave.SlaveServer;
  * Created by gary on 08/01/15.
  */
 public class ApplicationTest {
+    private static int SLEEP_TIME = 2000;
 
     @org.junit.Before
     public void setUp() {
@@ -19,8 +20,18 @@ public class ApplicationTest {
     @org.junit.Test
     public void testMains() throws Exception {
         SlaveServer.main(new String[]{"9001", "9002"});
+        sleep();
+
         LoadBalancer.main(new String[]{"5000", "lcf.xml"});
+        sleep();
+
         Client client = new Client("Gary", "http://localhost:5000/RPC2");
         System.out.println(client.call("A quick brown fox jumps over a lazy dog"));
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(SLEEP_TIME);
+        } catch (Exception e) {}
     }
 }
