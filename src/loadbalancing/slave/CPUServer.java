940f6386b483c023cbe37bd010dc5326db52600d
Gary Ye <gye@student.tgm.ac.at>
2015-01-09 09:00:47 +0100

    Pushed a server that will use the CPU strongly

---
+++ src/loadbalancing/slave/CPUServer.java
@@ -0,0 +1,41 @@
+package loadbalancing.slave;
+
+import org.apache.log4j.Logger;
+
+/**
+ * The CPU server receives the string and searches with an O(n^4) naive algorithm
+ * for the longest substring that occurs twice in the string. If there are multiple
+ * then the lexicographical smallest will be chosen.
+ */
+public class CPUServer extends SlaveServer {
+    private static Logger log = Logger.getLogger(SlaveServer.class.getName());
+
+    /**
+     * Initializes a new slave server object with a given port number.
+     *
+     * @param port the port number
+     */
+    public CPUServer(int port) {
+        super(port);
+    }
+
+    @Override
+    public String call(String request) throws Exception {
+        int n = request.length();
+        String ret = "";
+        for (int len = 0; len < n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                for (int j = i + 1; j + len - 1 < n; j++) {
+                    if (request.substring(i, len).equals(request.substring(j, len))) {
+                        // check whether our current result has a shorter length or is lexicographically greater
+                        if (ret.length() < len || (ret.length() == len && ret.compareTo(request.substring(i, len)) >
+                                0)) {
+                            ret = request.substring(i, len);
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+}
